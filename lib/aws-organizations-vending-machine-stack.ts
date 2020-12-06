ba9e35b026f28efc731287eb3cbfb9e2237035b7
Soenke Ruempler <soenke.ruempler@superluminar.io>
2020-12-06 10:10:21 +0100

    simplify to one file

--- lib/aws-organizations-vending-machine-stack.ts
+++ lib/aws-organizations-vending-machine-stack.ts
@@ -3,6 +3,8 @@ import {ApiToStateMachine} from "./api-to-state-machine";
 import {StateMachine, StateMachineType} from '@aws-cdk/aws-stepfunctions';
 import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
 import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import {Role, ServicePrincipal} from "@aws-cdk/aws-iam";
+import {CfnResource} from "@aws-cdk/core";
 
 export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -27,10 +29,45 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
             }
         )
 
+        const apiRole = new Role(this, `${id}Role`, {
+            assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
+        });
+        stateMachine.grant(apiRole, 'states:StartSyncExecution')
+
         this.templateOptions.transforms = ['AWS::Serverless-2016-10-31'];
-        // The code that defines your stack goes here
-        new ApiToStateMachine(this, "Api", {
-            stateMachine: stateMachine
+        new CfnResource(this, 'HttpApi', {
+            type: "AWS::Serverless::HttpApi",
+            properties: {
+                DefinitionBody: {
+                    "openapi": "3.0.1",
+                    "info": {
+                        "title": "AwsOrganizationsVendingMachine",
+                        "version": "2020-11-06 15:32:29UTC"
+                    },
+                    "paths": {
+                        "/": {
+                            "post": {
+                                "responses": {
+                                    "default": {
+                                        "description": "Default response for POST /"
+                                    }
+                                },
+                                "x-amazon-apigateway-integration": {
+                                    "integrationSubtype": "StepFunctions-StartSyncExecution",
+                                    "credentials": apiRole.roleArn,
+                                    "requestParameters": {
+                                        "StateMachineArn": stateMachine.stateMachineArn,
+                                    },
+                                    "payloadFormatVersion": "1.0",
+                                    "type": "aws_proxy",
+                                    "connectionType": "INTERNET"
+                                }
+                            }
+                        }
+                    },
+                    "x-amazon-apigateway-importexport-version": "1.0"
+                }
+            }
         })
     }
 }
