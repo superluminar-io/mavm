bfd90391209a5caf538c17fd7205b5d3222f1c18
Soenke Ruempler <soenke.ruempler@superluminar.io>
2020-12-15 16:49:42 +0100

    missing ddb permissions

--- lib/aws-organizations-vending-machine-stack.ts
+++ lib/aws-organizations-vending-machine-stack.ts
@@ -29,7 +29,7 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
         canary.role.addToPrincipalPolicy(new PolicyStatement(
             {
                 resources: ['*'],
-                actions: ['secretsmanager:GetSecretValue', 'ssm:*Parameter*', 'sqs:*', 's3:*', 'transcribe:*'], // TODO: least privilege
+                actions: ['secretsmanager:GetSecretValue', 'ssm:*Parameter*', 'sqs:*', 's3:*', 'transcribe:*', 'dynamodb:*'], // TODO: least privilege
             }
         ));
 
