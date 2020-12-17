0f4d48222e3ab527e4e34e3fb89f964a6bf5907c
Soenke Ruempler <soenke.ruempler@superluminar.io>
2020-12-17 11:11:48 +0100

    remove unused code

--- code/gen-id.ts
+++ code/gen-id.ts
@@ -1,21 +1,13 @@
-//import * as AWS from 'aws-sdk';
-//const cws: AWS.Synthetics = new AWS.Synthetics();
 const { v4: uuidv4 } = require('uuid');
 const util = require('util');
 
 exports.handler = async (event: any, context: any, callback: any) => {
-    // @ts-ignore
     const randomSuffix = uuidv4().split('-')[0];
     const ACCOUNT_EMAIL = `superwerker-aws-test+${randomSuffix}@superluminar.io`; // TODO: this has to be generated from a subdomain which is under control so we can close the account automatically
     const ACCOUNT_NAME = util.format('ovm-%s', randomSuffix);
 
-    //await cws.startCanary({Name: process.env['CANARY_NAME']}).promise()
     return {
         'accountEmail': ACCOUNT_EMAIL,
         'accountName': ACCOUNT_NAME,
     };
-    callback(null, {
-        'accountEmail': ACCOUNT_EMAIL,
-        'accountName': ACCOUNT_NAME,
-    });
 }
