3a93f64a1be305ac4a8178c602320deca270b211
Soenke Ruempler <soenke.ruempler@superluminar.io>
2021-06-04 09:15:40 +0200

    chore: remove old account creation

--- lib/aws-organizations-vending-machine-stack.ts
+++ lib/aws-organizations-vending-machine-stack.ts
@@ -1,28 +1,20 @@
 import * as cdk from '@aws-cdk/core';
-import {CfnOutput, IgnoreMode} from '@aws-cdk/core';
 import * as lambda_core from '@aws-cdk/aws-lambda';
 import * as lambda from '@aws-cdk/aws-lambda-nodejs';
-import * as cws from '@aws-cdk/aws-synthetics';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as iam from '@aws-cdk/aws-iam';
 import * as restapi from '@aws-cdk/aws-apigateway';
 import * as httpapi from '@aws-cdk/aws-apigatewayv2';
 import * as httpapiint from '@aws-cdk/aws-apigatewayv2-integrations';
-import * as events from '@aws-cdk/aws-events';
-import * as targets from '@aws-cdk/aws-events-targets';
 import * as lambdaeventsources from '@aws-cdk/aws-lambda-event-sources';
 import * as sfn from '@aws-cdk/aws-stepfunctions';
 import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
-import * as cw from '@aws-cdk/aws-cloudwatch';
-import * as cw_actions from '@aws-cdk/aws-cloudwatch-actions';
-import * as sns from '@aws-cdk/aws-sns';
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import {Asset} from '@aws-cdk/aws-s3-assets';
 
 import {PolicyStatement} from "@aws-cdk/aws-iam";
 import * as path from "path";
-import * as fs from "fs";
 
 export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -40,16 +32,6 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
         const invoiceCurrency = new cdk.CfnParameter(this, "InvoiceCurrency", {
             type: "String",
             description: "Currency for billing/invoice.",
-        });
-
-        const mavmAlarmsSnsTopic = new sns.Topic(this, 'MavmAlarmsSnsTopic');
-
-        const accountCreationQueueDlq = new sqs.Queue(this, 'AccountCreationDLQueue');
-        const accountCreationQueue = new sqs.Queue(this, 'AccountCreationQueue', {
-            deadLetterQueue: {
-                queue: accountCreationQueueDlq,
-                maxReceiveCount: 1, // if it didn't work, throw it away. The CWS canary monitoring throws an alarm on too many failures. (TODO: implement alarm)
-            }
         });
 
         const creditCard3SecureQueue = new sqs.Queue(this, 'CreditCard3SecureQueue', {
@@ -107,37 +89,6 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
             { methodResponses: [{ statusCode: "200" }] }
         );
 
-        const canary = new cws.Canary(this, 'CreateAccount', {
-            runtime: new cws.Runtime('syn-nodejs-2.2'),
-            test: cws.Test.custom({
-                code: cws.Code.fromInline(fs.readFileSync(path.join(__dirname, '../code/create.js'), {encoding: "utf-8"})),
-                handler: 'index.handler',
-            }),
-            startAfterCreation: true,
-
-            // start it regularly, this actually fakes a "watchdog" / "angel" process which keeps account creation running
-            schedule: cws.Schedule.expression('rate(1 hour)'),
-        });
-
-        canary.role.addToPrincipalPolicy(new PolicyStatement(
-            {
-                resources: ['*'],
-                actions: ['secretsmanager:GetSecretValue', 'ssm:*Parameter*', 'sqs:*', 's3:*', 'transcribe:*', 'dynamodb:*', 'sts:*'], // TODO: least privilege
-            }
-        ));
-
-        // work around https://github.com/aws/aws-cdk/pull/11865
-        const cfnCanary = canary.node.defaultChild as cws.CfnCanary;
-        cfnCanary.addOverride('Properties.RunConfig.EnvironmentVariables', {
-            PRINCIPAL: process.env.CDK_DEFAULT_ACCOUNT,
-            QUEUE_URL: accountCreationQueue.queueUrl,
-            INVOICE_CURRENCY: invoiceCurrency.valueAsString,
-            INVOICE_EMAIL: invoiceEmail.valueAsString,
-
-        });
-        cfnCanary.addOverride('Properties.RunConfig.TimeoutInSeconds', 600); // delete me after https://github.com/aws/aws-cdk/pull/11865 can be used
-
-        // NEW ACCOUNT CREATION START
         const createAccountCodeAsset = new Asset(this, 'CreateAccountCodeAsset', {
             path: path.join(__dirname, '../code/create-account'),
             exclude: [
@@ -248,27 +199,6 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
                 actions: ['dynamodb:*'], // TODO: least privilege
             }
         ));
-
-        const vendNewAccountsFunction = new lambda.NodejsFunction(this, 'VendNewAccountsFunction', {
-            entry: 'code/vend-new-accounts.ts',
-            environment: {
-                QUEUE_URL: accountCreationQueue.queueUrl,
-                VENDING_CEILING: vendingCeiling.valueAsString,
-            },
-            timeout: cdk.Duration.minutes(1),
-        });
-        vendNewAccountsFunction.addToRolePolicy(new PolicyStatement(
-            {
-                resources: ['*'],
-                actions: ['dynamodb:*', 'states:*', 'sqs:sendMessage'], // TODO: least privilege
-            }
-        ));
-        new events.Rule(this, 'Rule', {
-            schedule: events.Schedule.expression('rate(' + vendingCeiling.valueAsString + ' hours)'),
-            targets: [
-                new targets.LambdaFunction(vendNewAccountsFunction)
-            ]
-        });
 
         api.addRoutes({
             path: '/vend',
