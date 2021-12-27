fe277e0761b55e4fc9a4bf7200f18507c5793a88
Soenke Ruempler <soenke.ruempler@superluminar.io>
2021-12-27 19:53:37 +0100

    chore: upgrade to cdkv2

--- bin/aws-organizations-vending-machine.ts
+++ bin/aws-organizations-vending-machine.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { AwsOrganizationsVendingMachineStack } from '../lib/aws-organizations-vending-machine-stack';
 
 const app = new cdk.App();
--- lib/aws-organizations-vending-machine-stack.ts
+++ lib/aws-organizations-vending-machine-stack.ts
@@ -1,23 +1,25 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda_core from '@aws-cdk/aws-lambda';
-import * as lambda from '@aws-cdk/aws-lambda-nodejs';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as dynamodb from '@aws-cdk/aws-dynamodb';
-import * as iam from '@aws-cdk/aws-iam';
-import * as restapi from '@aws-cdk/aws-apigateway';
-import * as httpapi from '@aws-cdk/aws-apigatewayv2';
-import * as httpapiint from '@aws-cdk/aws-apigatewayv2-integrations';
-import * as lambdaeventsources from '@aws-cdk/aws-lambda-event-sources';
-import * as sfn from '@aws-cdk/aws-stepfunctions';
-import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
-import * as codebuild from '@aws-cdk/aws-codebuild';
-import {Asset} from '@aws-cdk/aws-s3-assets';
+import { Stack } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
-import {PolicyStatement} from "@aws-cdk/aws-iam";
+import { aws_iam as iam } from 'aws-cdk-lib';
+import { aws_sqs as sqs } from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_lambda_nodejs as lambda_nodejs } from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
+import { aws_apigateway as restapi } from 'aws-cdk-lib';
+import * as httpapi from '@aws-cdk/aws-apigatewayv2-alpha';
+import { aws_lambda_event_sources as lambdaeventsources } from 'aws-cdk-lib';
+import { aws_stepfunctions as sfn } from 'aws-cdk-lib';
+import { aws_stepfunctions_tasks as tasks } from 'aws-cdk-lib';
+import { aws_codebuild as codebuild } from 'aws-cdk-lib';
+import { aws_s3_assets as s3_assets } from 'aws-cdk-lib';
+import * as httpapiint from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+
 import * as path from "path";
 
-export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class AwsOrganizationsVendingMachineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const vendingCeiling = new cdk.CfnParameter(this, "VendingCeiling", {
@@ -89,7 +91,7 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
             { methodResponses: [{ statusCode: "200" }] }
         );
 
-        const createAccountCodeAsset = new Asset(this, 'CreateAccountCodeAsset', {
+        const createAccountCodeAsset = new s3_assets.Asset(this, 'CreateAccountCodeAsset', {
             path: path.join(__dirname, '../code/create-account'),
             exclude: [
                 'node_modules',
@@ -110,7 +112,7 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
                 QUEUE_URL_3D_SECURE: {value: creditCard3SecureQueue.queueUrl},
             }
         });
-        createAccountCodeProject.role?.addToPrincipalPolicy(new PolicyStatement(
+        createAccountCodeProject.role?.addToPrincipalPolicy(new iam.PolicyStatement(
             {
                 resources: ['*'],
                 actions: ['secretsmanager:GetSecretValue', 'ssm:*Parameter*', 'sqs:*', 's3:*', 'transcribe:*', 'dynamodb:*', 'sts:*'], // TODO: least privilege
@@ -138,14 +140,14 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
 
         createAccountStateMachineAccountCreationStep.addCatch(createAccountStateMachineWaitStep);
 
-        const accountNameProviderFunction = new lambda.NodejsFunction(this, 'AccountNameProviderFunction', {
+        const accountNameProviderFunction = new lambda_nodejs.NodejsFunction(this, 'AccountNameProviderFunction', {
             entry: 'code/account-name-provider.ts',
             environment: {
                 VENDING_CEILING: vendingCeiling.valueAsString,
             },
             timeout: cdk.Duration.minutes(1),
         });
-        accountNameProviderFunction.addToRolePolicy(new PolicyStatement(
+        accountNameProviderFunction.addToRolePolicy(new iam.PolicyStatement(
             {
                 resources: ['*'],
                 actions: ['dynamodb:*'], // TODO: least privilege
@@ -192,10 +194,10 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
 
         const api = new httpapi.HttpApi(this, "OrgVendingApi");
 
-        const getAvailableAccountFunction = new lambda.NodejsFunction(this, 'GetAvailableAccountFunction', {
+        const getAvailableAccountFunction = new lambda_nodejs.NodejsFunction(this, 'GetAvailableAccountFunction', {
             entry: 'code/get-available-account.ts',
         });
-        getAvailableAccountFunction.addToRolePolicy(new PolicyStatement(
+        getAvailableAccountFunction.addToRolePolicy(new iam.PolicyStatement(
             {
                 resources: ['*'],
                 actions: ['dynamodb:*'], // TODO: least privilege
@@ -205,12 +207,10 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
         api.addRoutes({
             path: '/vend',
             methods: [ httpapi.HttpMethod.GET ],
-            integration: new httpapiint.LambdaProxyIntegration({
-                handler: getAvailableAccountFunction,
-            }),
+            integration: new httpapiint.HttpLambdaIntegration('GetAvailableAccountHandler', getAvailableAccountFunction),
         });
 
-        const closeAccountCodeAsset = new Asset(this, 'CloseAccountCodeAsset', {
+        const closeAccountCodeAsset = new s3_assets.Asset(this, 'CloseAccountCodeAsset', {
             path: path.join(__dirname, '../code/close-account'),
             exclude: [
                 'node_modules',
@@ -225,7 +225,7 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
                 path: closeAccountCodeAsset.s3ObjectKey,
             }),
         });
-        closeAccountCodeCodeBuild.role?.addToPrincipalPolicy(new PolicyStatement(
+        closeAccountCodeCodeBuild.role?.addToPrincipalPolicy(new iam.PolicyStatement(
             {
                 resources: ['*'],
                 actions: ['secretsmanager:GetSecretValue', 'dynamodb:*', 'sts:AssumeRole'], // TODO: least privilege
@@ -264,20 +264,20 @@ export class AwsOrganizationsVendingMachineStack extends cdk.Stack {
             }
         );
 
-        const queueAccountDeletionFunction = new lambda.NodejsFunction(this, 'QueueActionDeletionFunction', {
+        const queueAccountDeletionFunction = new lambda_nodejs.NodejsFunction(this, 'QueueActionDeletionFunction', {
             entry: 'code/queue-account-deletion.ts',
             environment: {
                 ACCOUNT_CLOSE_STATE_MACHINE_ARN: accountDeletionStateMachineNew.stateMachineArn
             }
         });
-        queueAccountDeletionFunction.addToRolePolicy(new PolicyStatement(
+        queueAccountDeletionFunction.addToRolePolicy(new iam.PolicyStatement(
             {
                 resources: ['*'],
                 actions: ['states:*'], // TODO: least privilege
             }
         ));
         queueAccountDeletionFunction.addEventSource(new lambdaeventsources.DynamoEventSource(table, {
-            startingPosition: lambda_core.StartingPosition.TRIM_HORIZON,
+            startingPosition: lambda.StartingPosition.TRIM_HORIZON,
         }));
 
     }
