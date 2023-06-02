9a9b352bfd9bed46819f7d5f5884ff252ba5cc50
Joscha <joscha.meyer@superluminar.io>
2023-06-02 12:40:23 +0200

    suspend accounts wip

--- bin/aws-organizations-vending-machine.ts
+++ bin/aws-organizations-vending-machine.ts
@@ -3,6 +3,8 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsOrganizationsVendingMachineStack } from '../lib/aws-organizations-vending-machine-stack';
 import {CleanUpMavmAccountsStack} from "../lib/clean-up-mavm-accounts-stack";
+import {SuspendBuriedAccountsStack} from "../lib/suspend-buried-accounts-stack";
 
 const app = new cdk.App();
 new CleanUpMavmAccountsStack(app, 'CleanUpMavmAccountsStack', {env: {region: 'eu-west-1'}});
+new SuspendBuriedAccountsStack(app, 'SuspendBuriedAccountsStack', {env: {region: 'eu-west-1'}});
---
+++ lib/suspend-buried-accounts-stack.ts
@@ -0,0 +1,87 @@
+import {
+  aws_iam as iam,
+  aws_stepfunctions as sfn,
+  aws_stepfunctions_tasks as tasks,
+  Stack,
+  StackProps
+} from "aws-cdk-lib";
+import {Construct} from "constructs";
+import {StateMachine} from "aws-cdk-lib/aws-stepfunctions";
+import {Table} from "aws-cdk-lib/aws-dynamodb";
+
+
+export class SuspendBuriedAccountsStack extends Stack {
+
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const superluminarOrgOVMRole = iam.Role.fromRoleArn(this, 'SuperluminarOrgOVMRole', 'arn:aws:iam::197726340368:role/OVM-invite-move-and-close-account-role');
+
+    const accounts = Table.fromTableName(this, 'MavmAccountsTable', 'account');
+
+    const suspendBuriedAccounts = new tasks.CallAwsService(this, 'QueryBuriedAccounts', {
+      service: 'dynamodb',
+      action: 'query',
+      iamResources: ['*'],
+      parameters: {
+        TableName: accounts.tableName,
+        IndexName: 'account_status',
+        KeyConditionExpression: 'account_status = :account_status',
+        ExpressionAttributeValues: {
+          ':account_status': {S: 'BURIED'}
+        }
+      },
+      resultPath: sfn.JsonPath.stringAt('$.queryResponse'),
+    });
+
+    const markAccountAsSuspended = new tasks.DynamoUpdateItem(this, 'MarkAccountAsSuspended', {
+      key: {
+        'account_name': tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.accountName'))
+      },
+      table: accounts,
+      expressionAttributeValues: {
+        ':account_status': tasks.DynamoAttributeValue.fromString('BURIED_AND_CLOSED'),
+        ':buried_and_close_date': tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$$.State.EnteredTime')),
+      },
+      updateExpression: 'SET account_status = :account_status, buried_and_close_date = :buried_and_close_date',
+      conditionExpression: 'attribute_not_exists(buried_and_close_date)',
+    });
+
+    // account already close exception AccountAlreadyClosedException
+    const suspendAccount = new tasks.CallAwsService(this, 'SuspendAccount', {
+      service: 'organizations',
+      action: 'closeAccount',
+      credentials: {role: sfn.TaskRole.fromRole(superluminarOrgOVMRole)},
+      iamResources: ['*'],
+      parameters: {
+        AccountId: sfn.JsonPath.stringAt('$.accountId'),
+      },
+      resultPath: sfn.JsonPath.stringAt('$.closeAccountResponse'),
+    }).addCatch(new sfn.Succeed(this, 'Exceeded the number of requests'), {
+      errors: ['Organizations.TooManyRequestsException'],
+    }).addCatch(new sfn.Choice(this, 'ReasonCloseAccountRequestsLimitExceeded')
+        .when(sfn.Condition.stringMatches(sfn.JsonPath.stringAt('$.lastError.Reason'), 'CloseAccountRequestsLimitExceeded'),
+          new sfn.Succeed(this, 'Exceeded the number of member accounts you can close concurrently.'))
+        .when(sfn.Condition.stringMatches(sfn.JsonPath.stringAt('$.lastError.Reason'), 'CloseAccountQuotaExceeded'),
+          new sfn.Succeed(this, 'Exceeded the number of member accounts you can close in a 30 day period.'))
+        .otherwise(markAccountAsSuspended), {
+      errors: ['Organizations.ConstraintViolationException'],
+      resultPath: sfn.JsonPath.stringAt('$.lastError'),
+    })
+      .next(markAccountAsSuspended);
+
+    suspendBuriedAccounts.next(new sfn.Map(this, 'MapBuriedAccounts', {
+      itemsPath: sfn.JsonPath.stringAt('$.queryResponse.Items'),
+      parameters: {
+        accountId: sfn.JsonPath.stringAt('$$.Map.Item.Value.account_id.S'),
+        accountName: sfn.JsonPath.stringAt('$$.Map.Item.Value.account_name.S'),
+        accountEmail: sfn.JsonPath.stringAt('$$.Map.Item.Value.account_email.S'),
+      },
+      maxConcurrency: 1,
+    }).iterator(suspendAccount));
+
+    new StateMachine(this, 'SuspendBuriedAccountsStateMachine', {
+      definition: suspendBuriedAccounts,
+    })
+  }
+}
